423e130b26c47b568718f050209982d214f9a811
DANGERSTEPHEN <DANGERSTEVE1@GMAILCOM>
2016-11-05 17:40:25 -0700

    added function to find winner and counter for score

--- main.js
+++ main.js
@@ -1,51 +1,31 @@
-// //test to see if JS is linked
-// console.log("JavaScript is connected!");
-//
-// //Q = 81 P = 80
-//
-// $(document).keydown(function(e) {
-//     if (e.keyCode == 81) {
-//         $('.car1').css('left', '+=10');
-//     }
-//     if (e.keyCode == 80) {
-//         $('.car2').css('left', '+=10');
-//     }
-// });
-
-var finishLine = 1000;
-var startLine = 10;
+var finishLine = 900;
+var startLine =  0;
 var $boat1 = $('.boat1');
 var $boat2 = $('.boat2');
+var boat1Score = 0;
+// $('.body').append(boat1Score);
+var boat2Score = 0;
+// $('.body').append(boat2Score);
 
 $('.btn-primary').on('click', function(event) {
-  document.location.reload(true);
+  $('.boat1').css('left', '0');
+  $('.boat2').css('left', '0');
   })
-
-// $(function(){
-// var car1 = '<div class="car1"></div>';
-// var car2 = '<div class="car2"></div>';
-// $('.track').append(car1);
-// $('.track').append(car2);
-
 $(document).keydown(function(e) {
-
-      if (e.keyCode == 80) {
-          $('.boat1').css('left', '+=10');
+      if (e.keyCode === 81) {
+          $('.boat1').css('left', '+=10px');
           var boat1 = $('.boat1').css('left');
-          // checkPosition($player1);
-          if(parseInt($boat1.css('left'), 10) > finishLine) {
-            alert("Phew! You made it!");
+          if(parseInt($boat1.css('left'), startLine) > finishLine) {
+            alert("YAY! Go Boat 1!");
+          $(".boat1Score").html(boat1Score+=1);
           }
           }
-
-      if(e.keyCode == 81 ) {
-      $('.boat2').css('left', '+=10');
+      if(e.keyCode === 80 ) {
+      $('.boat2').css('left', '+=10px');
       var boat2 = $('.boat2').css('left');
-      // checkPosition($player2);
-        if((parseInt($boat2.css('left'), 10) > finishLine)) {
-          alert("Better luck next time human!");
-          // document.getElementById("message").innerHTML = "Better luck next time human!";
+        if((parseInt($boat2.css('left'), startLine) > finishLine)) {
+          alert("Congrats boat 2, you won!");
+          $(".boat2Score").html(boat2Score+=1);
         }
       }
-
  });

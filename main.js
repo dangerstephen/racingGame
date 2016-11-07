d3d149487542a32332da11f11129374de31e70c2
DANGERSTEPHEN <DANGERSTEVE1@GMAILCOM>
2016-11-06 21:22:18 -0800

    added score counter

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var finishLine = 900;
-var startLine =  0;
+var startLine = 0;
 var $boat1 = $('.boat1');
 var $boat2 = $('.boat2');
 var boat1Score = 0;
@@ -8,24 +8,24 @@ var boat2Score = 0;
 // $('.body').append(boat2Score);
 
 $('.btn-primary').on('click', function(event) {
-  $('.boat1').css('left', '0');
-  $('.boat2').css('left', '0');
-  })
+    $('.boat1').css('left', '0');
+    $('.boat2').css('left', '0');
+})
 $(document).keydown(function(e) {
-      if (e.keyCode === 81) {
-          $('.boat1').css('left', '+=10px');
-          var boat1 = $('.boat1').css('left');
-          if(parseInt($boat1.css('left'), startLine) > finishLine) {
+    if (e.keyCode === 81) {
+        $('.boat1').css('left', '+=10px');
+        var boat1 = $('.boat1').css('left');
+        if (parseInt($boat1.css('left'), startLine) > finishLine) {
             alert("YAY! Go Boat 1!");
-          $(".boat1Score").html(boat1Score+=1);
-          }
-          }
-      if(e.keyCode === 80 ) {
-      $('.boat2').css('left', '+=10px');
-      var boat2 = $('.boat2').css('left');
-        if((parseInt($boat2.css('left'), startLine) > finishLine)) {
-          alert("Congrats boat 2, you won!");
-          $(".boat2Score").html(boat2Score+=1);
+            $(".boat1Score").html(boat1Score += 1);
         }
-      }
- });
+    }
+    if (e.keyCode === 80) {
+        $('.boat2').css('left', '+=10px');
+        var boat2 = $('.boat2').css('left');
+        if ((parseInt($boat2.css('left'), startLine) > finishLine)) {
+            alert("Congrats boat 2, you won!");
+            $(".boat2Score").html(boat2Score += 1);
+        }
+    }
+});
